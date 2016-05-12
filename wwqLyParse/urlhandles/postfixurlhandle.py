0cb6cacb6831d94a0eb7650598511e41eac0b18e
wwqgtxx <wwqgtxx@gmail.com>
2016-05-12 22:33:23 +0800

    fix PostfixUrlHandle and make it multithread
    Signed-off-by: wwqgtxx <wwqgtxx@gmail.com>

--- wwqLyParse/urlhandles/postfixurlhandle.py
+++ wwqLyParse/urlhandles/postfixurlhandle.py
@@ -3,7 +3,7 @@
 # author wwqgtxx <wwqgtxx@gmail.com>
 
 
-import urllib.request,io,os,sys,json,re
+import urllib.request,io,os,sys,json,re,threading,queue
 
 from pyquery.pyquery import PyQuery
 
@@ -18,8 +18,21 @@ class PostfixUrlHandle(common.UrlHandle):
     filters = ['^(http|https)://[^\s]+/[^\s]+\.[s]{0,1}html(\?|#)[^\s]+']
         
     def urlHandle(self,url):
+        def getUrl(queue,url):
+            queue.put(common.getUrl(url))
         result =  re.match('^(http|https)://[^\s]+/[^\s]+\.[s]{0,1}html',url).group()
-        print('urlHandle:"'+url+'"-->"'+result+'"')
-        return result
-
+        q_results = queue.Queue()
+        htmls = []
+        t1 = threading.Thread(target=getUrl, args=(q_results, url))
+        t2 = threading.Thread(target=getUrl, args=(q_results, result))
+        t1.start()
+        t2.start()
+        t1.join()
+        t2.join()
+        while not q_results.empty():
+            htmls.append(q_results.get())
+        if htmls[0] == htmls[1]:
+            print('urlHandle:"'+url+'"-->"'+result+'"')
+            return result
+        return url
     

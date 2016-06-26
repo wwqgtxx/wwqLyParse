5ef5d950cdcb2626f9cae5a417426938f891b2f9
wwqgtxx <wwqgtxx@gmail.com>
2016-06-26 18:24:52 +0800

    update git submodule
    Signed-off-by: wwqgtxx <wwqgtxx@gmail.com>

--- wwqLyParse/submoduleupdate.bat
+++ wwqLyParse/submoduleupdate.bat
@@ -1,2 +1,9 @@
-git submodule foreach git pull
+@rem git submodule foreach git pull
+cd ykdl
+git pull --rebase https://github.com/zhangn1985/ykdl.git master
+git push --force
+cd..
+cd you-get
+git pull --rebase https://github.com/soimort/you-get.git master
+git push --force
 @pause

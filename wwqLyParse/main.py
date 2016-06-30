eff05c66f038334be863238fb4fc1331fcc3647e
wwqgtxx <wwqgtxx@gmail.com>
2016-06-30 23:27:28 +0800

    import pyquery and flask in printok.py to sure that library can be imported

--- wwqLyParse/main.py
+++ wwqLyParse/main.py
@@ -49,7 +49,7 @@ except Exception:
 version = {
     'port_version' : "0.5.0", 
     'type' : 'parse', 
-    'version' : '0.4.3',
+    'version' : '0.4.4',
     'uuid' : '{C35B9DFC-559F-49E2-B80B-79B66EC77471}',
     'filter' : [],
     'name' : 'WWQ猎影解析插件', 
--- wwqLyParse/printok.py
+++ wwqLyParse/printok.py
@@ -2,6 +2,18 @@
 # -*- coding: utf-8 -*-
 # author wwqgtxx <wwqgtxx@gmail.com>
 
+import pyquery
+
+try:
+    from .lib import bridge
+except Exception as e:
+    from lib import bridge
+import sys
+sys.path.insert(0, bridge.pn(bridge.pjoin(bridge.get_root_path(), './lib/flask_lib')))
+try:
+    from flask import Flask,request
+except Exception:
+    from .flask import Flask,request
 
 if __name__ == "__main__":
     print("ok")

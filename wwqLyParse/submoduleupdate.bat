d96bcd6adfdbf79d8c7042d358d7084da6d4c21c
wwqgtxx <wwqgtxx@gmail.com>
2016-07-03 23:27:17 +0800

    update submodule code
    Signed-off-by: wwqgtxx <wwqgtxx@gmail.com>

--- wwqLyParse/submoduleupdate.bat
+++ wwqLyParse/submoduleupdate.bat
@@ -4,7 +4,7 @@ git pull https://github.com/zhangn1985/ykdl.git master
 git push --force
 cd..
 cd you-get
-git pull https://github.com/soimort/you-get.git master
+git pull https://github.com/soimort/you-get.git develop
 git push --force
 cd..
 cd p_video

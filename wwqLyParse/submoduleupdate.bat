93b17dc834f318b32eafc4283c2c74d4981649c0
wwqgtxx <wwqgtxx@gmail.com>
2016-07-03 23:23:08 +0800

    update submodule code
    Signed-off-by: wwqgtxx <wwqgtxx@gmail.com>

--- wwqLyParse/submoduleupdate.bat
+++ wwqLyParse/submoduleupdate.bat
@@ -1,13 +1,13 @@
 @rem git submodule foreach git pull
 cd ykdl
-git pull --rebase https://github.com/zhangn1985/ykdl.git master
+git pull https://github.com/zhangn1985/ykdl.git master
 git push --force
 cd..
 cd you-get
-git pull --rebase https://github.com/soimort/you-get.git master
+git pull https://github.com/soimort/you-get.git master
 git push --force
 cd..
 cd p_video
-git pull --rebase https://github.com/sceext2/p_video.git pv-8
+git pull https://github.com/sceext2/p_video.git pv-8
 git push --force
 @pause

2e9ba97258422fb877532910add2c5af1fa5356d
wwqgtxx <wwqgtxx@gmail.com>
2018-03-23 10:51:29 +0800

    continue update submodule and update lru cache

--- wwqLyParse/common/lru_cache.py
+++ wwqLyParse/common/lru_cache.py
@@ -8,37 +8,47 @@ import collections
 import threading
 import time
 
+___all___ = ["LRUCache"]
+
+
+class NullLock(object):
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        return
+
 
 class LRUCache(collections.MutableMapping):
-    def __init__(self, size=5, timeout=60, *args, **kwargs):
+    def __init__(self, size=5, timeout=60, delete_handle=None, use_lock=True, *args, **kwargs):
         self.size = size
         self.timeout = timeout
         self._store = {}
         self._keys_to_last_time = collections.OrderedDict()
-        self._lock = threading.RLock()
+        self._lock = threading.RLock() if use_lock else NullLock()
+        self.delete_handle = delete_handle
         self.update(dict(*args, **kwargs))
+
+    def flush(self, key):
+        with self._lock:
+            self.sweep()
+            t = time.time()
+            self._keys_to_last_time[key] = t
+            self._keys_to_last_time.move_to_end(key, False)  # move to start
 
     def __getitem__(self, key):
         with self._lock:
-            self.sweep()
-            t = time.time()
-            item = self._store[key]
-            self._keys_to_last_time[key] = t
-            self._keys_to_last_time.move_to_end(key, False)  # move to start
-            return item
+            self.flush(key)
+            return self._store[key]
 
     def __setitem__(self, key, value):
         with self._lock:
-            self.sweep()
-            t = time.time()
+            self.flush(key)
             self._store[key] = value
-            self._keys_to_last_time[key] = t
-            self._keys_to_last_time.move_to_end(key, False)  # move to start
 
     def __delitem__(self, key):
         with self._lock:
-            del self._store[key]
-            del self._keys_to_last_time[key]
+            self._delete(key, call_handle=False)
 
     def __iter__(self):
         with self._lock:
@@ -47,6 +57,18 @@ class LRUCache(collections.MutableMapping):
     def __len__(self):
         with self._lock:
             return len(self._store)
+
+    def items(self):
+        with self._lock:
+            return self._store.items()
+
+    def values(self):
+        with self._lock:
+            return self._store.values()
+
+    def keys(self):
+        with self._lock:
+            return self._store.keys()
 
     def sweep(self):
         with self._lock:
@@ -62,13 +84,18 @@ class LRUCache(collections.MutableMapping):
                     if i is None:
                         break
                     k, v = i
-                    del self._store[k]
-                    del self._keys_to_last_time[k]
+                    self._delete(k)
 
             while len(sorted_dict_items) > 0 and now - sorted_dict_items[-1][1] > self.timeout:
-                k, v = sorted_dict_items.pop()
-                del self._store[k]
-                del self._keys_to_last_time[k]
+                i = sorted_dict_items.pop()
+                k, v = i
+                self._delete(k)
+
+    def _delete(self, key, call_handle=True):
+        if call_handle and self.delete_handle:
+            self.delete_handle((key, self._store[key]))
+        del self._store[key]
+        del self._keys_to_last_time[key]
 
 
 if __name__ == '__main__':

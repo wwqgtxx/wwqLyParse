5528c5d7f3e099e5345ca3f4fe7e08a1c6d147e3
wwqgtxx <wwqgtxx@gmail.com>
2018-06-30 21:31:56 +0800

    update bound concurent_futures to python37's

--- wwqLyParse/common/concurrent_futures/__init__.py
+++ wwqLyParse/common/concurrent_futures/__init__.py
@@ -10,9 +10,50 @@ from ._base import (FIRST_COMPLETED,
                     ALL_COMPLETED,
                     CancelledError,
                     TimeoutError,
+                    BrokenExecutor,
                     Future,
                     Executor,
                     wait,
                     as_completed)
-from .process import ProcessPoolExecutor
-from .thread import ThreadPoolExecutor
+
+__all__ = (
+    'FIRST_COMPLETED',
+    'FIRST_EXCEPTION',
+    'ALL_COMPLETED',
+    'CancelledError',
+    'TimeoutError',
+    'BrokenExecutor',
+    'Future',
+    'Executor',
+    'wait',
+    'as_completed',
+    'ProcessPoolExecutor',
+    'ThreadPoolExecutor',
+)
+
+
+def __dir__():
+    return __all__ + ('__author__', '__doc__')
+
+
+def __getattr__(name):
+    global ProcessPoolExecutor, ThreadPoolExecutor
+
+    if name == 'ProcessPoolExecutor':
+        from .process import ProcessPoolExecutor as pe
+        ProcessPoolExecutor = pe
+        return pe
+
+    if name == 'ThreadPoolExecutor':
+        from .thread import ThreadPoolExecutor as te
+        ThreadPoolExecutor = te
+        return te
+
+    raise AttributeError("module {} has no attribute {}".format(__name__, name))
+
+
+import sys
+
+if sys.version_info[0:2] < (3, 7):
+    from .process import ProcessPoolExecutor
+    from .thread import ThreadPoolExecutor

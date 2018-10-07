5bc0277deaaa9ffff02f96dee6271ab3aa0f32cc
wwqgtxx <wwqgtxx@gmail.com>
2018-10-07 12:42:11 +0800

    continue work for IgnoreCaseDict

--- wwqLyParse/common/ignore_case_dict.py
+++ wwqLyParse/common/ignore_case_dict.py
@@ -5,42 +5,40 @@
 import collections
 
 
+def _return_item(item):
+    if isinstance(item, collections.MutableMapping):
+        return IgnoreCaseDict(item)
+    elif isinstance(item, collections.MutableSequence):
+        return MutableSequenceProxy(item)
+    return item
+
+
 class IgnoreCaseDict(collections.MutableMapping):
     def __init__(self, raw_dict: collections.MutableMapping):
         self.raw_dict = raw_dict
 
-    def _return_item(self, item):
-        if isinstance(item, collections.MutableMapping):
-            return self.__class__(item)
-        elif isinstance(item, collections.MutableSequence):
-            return [self._return_item(i) for i in item]
-        return item
+    def _find_key(self, k):
+        if k in self.raw_dict:
+            return k
+        if isinstance(k, str):
+            k_l = k.lower()
+            if k_l in self.raw_dict:
+                return k_l
+            for _k in self.raw_dict.keys():
+                if isinstance(_k, str):
+                    _k_l = _k.lower()
+                    if _k_l == k_l:
+                        return _k
+        return k
 
     def __setitem__(self, k, v):
-        k_l = str(k).lower()
-        for _k in self.raw_dict.keys():
-            _k_l = str(_k).lower()
-            if _k_l == k_l:
-                self.raw_dict[_k] = v
-                return
-        self.raw_dict[k] = v
+        self.raw_dict[self._find_key(k)] = v
 
     def __delitem__(self, k):
-        k_l = str(k).lower()
-        for _k in self.raw_dict.keys():
-            _k_l = str(_k).lower()
-            if _k_l == k_l:
-                del self.raw_dict[_k]
-                return
-        raise KeyError(k)
+        del self.raw_dict[self._find_key(k)]
 
     def __getitem__(self, k):
-        k_l = str(k).lower()
-        for _k in self.raw_dict.keys():
-            _k_l = str(_k).lower()
-            if _k_l == k_l:
-                return self._return_item(self.raw_dict[_k])
-        raise KeyError(k)
+        return _return_item(self.raw_dict[self._find_key(k)])
 
     def __len__(self):
         return len(self.raw_dict)
@@ -53,3 +51,32 @@ class IgnoreCaseDict(collections.MutableMapping):
 
     def __str__(self):
         return str(self.raw_dict)
+
+
+class MutableSequenceProxy(collections.MutableSequence):
+    def __init__(self, raw_seq: collections.MutableSequence):
+        self.raw_seq = raw_seq
+
+    def insert(self, index, o):
+        self.raw_seq.insert(index, o)
+
+    def __setitem__(self, i, o):
+        self.raw_seq[i] = o
+
+    def __delitem__(self, i):
+        del self.raw_seq[i]
+
+    def __getitem__(self, i):
+        return _return_item(self.raw_seq[i])
+
+    def __len__(self):
+        return len(self.raw_seq)
+
+    def __repr__(self):
+        return repr(self.raw_seq)
+
+    def __str__(self):
+        return str(self.raw_seq)
+
+
+__all__ = ["IgnoreCaseDict"]
